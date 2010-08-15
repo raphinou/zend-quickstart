1663bf85212f2e4e8620300898871c992ec87506
rb <rb@stib.(none)>
2010-08-15 12:18:20 +0200

    implemented sign-action

--- application/controllers/GuestbookController.php
+++ application/controllers/GuestbookController.php
@@ -11,10 +11,28 @@ class GuestbookController extends Zend_Controller_Action
     public function indexAction()
     {
         $guestbook = new Application_Model_GuestbookMapper();
-        $this->view->entries = $guestbook->fetchAll();
+                $this->view->entries = $guestbook->fetchAll();
+    }
 
+    public function signAction()
+    {
+       $request = $this->getRequest();
+        $form    = new Application_Form_Guestbook();
+ 
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $comment = new Application_Model_Guestbook($form->getValues());
+                $mapper  = new Application_Model_GuestbookMapper();
+                $mapper->save($comment);
+                return $this->_helper->redirector('index');
+            }
+        }
+ 
+        $this->view->form = $form;
     }
 
 
 }
+
+
 
